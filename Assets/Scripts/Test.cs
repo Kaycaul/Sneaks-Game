985875b218a11df7d9ca1088d1d7c31075c268af
Kaycaul <84094849+Kaycaul@users.noreply.github.com>
2023-05-02 12:33:15 -0400

    some testing with movement to remember how this works

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,14 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Test : MonoBehaviour {
+  
+    Rigidbody2D rb;
+
+    [SerializeField]
+    float speed = 48;
+    [SerializeField]
+    float topSpeed = 35;
+    [SerializeField]
+    float drag = 6;
 
     void Start() {
-
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
     }
 
-    void Update() {
-        // test code to spam console with a string
-        Debug.Log("Test");
+    void FixedUpdate() {
+        // get a vector representing the player input
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (input.magnitude > 1) {
+            input.Normalize();
+        }
+        // add force to the player
+        rb.AddForce(input * speed); 
+        // add drag
+        rb.AddForce(-rb.velocity * drag);
     }
 }
