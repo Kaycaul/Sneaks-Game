0cc79bd5adb813616c3c731b16e475e9ee5a9155
Kaycaul <84094849+Kaycaul@users.noreply.github.com>
2023-05-19 11:36:47 -0400

    background fading

--- Assets/Scripts/BackgroundManager.cs
+++ Assets/Scripts/BackgroundManager.cs
@@ -10,10 +10,37 @@ public class BackgroundManager : MonoBehaviour {
     public static BackgroundManager instance;
 
     Image background;
+    bool fading = false;
 
     // create a new background image with the provided sprite
     public static void UpdateBackground(Sprite sprite) {
         instance.background.sprite = sprite;
+    }
+
+    public static void FadeIn() {
+        if (isFading()) return;
+        instance.fading = true;
+        instance.StartCoroutine(instance.FadeBetween(Color.black, Color.white));
+    }
+
+    public static void FadeOut() {
+        if (isFading()) return;
+        instance.fading = true;
+        instance.StartCoroutine(instance.FadeBetween(Color.white, Color.black));
+    }
+
+    public static bool isFading() {
+        return instance.fading;
+    }
+
+    IEnumerator FadeBetween(Color start, Color end) {
+        float t = 0f;
+        while (t < 1f) {
+            instance.background.color = Color.Lerp(start, end, t);
+            t += Time.deltaTime;
+            yield return null;
+        }
+        fading = false;
     }
 
     private void Awake() {
--- Assets/Scripts/MotorcycleNPC.cs
+++ Assets/Scripts/MotorcycleNPC.cs
@@ -11,22 +11,14 @@ public class MotorcycleNPC : MonoBehaviour {
     [SerializeField] Sprite outside;
     [SerializeField] Sprite outside2;
 
-    /*
-    PlayerMovementTest player;
-    // when the player enters this collider
-    private void OnTriggerEnter2D(Collider2D other) {
-        player = other.GetComponent<PlayerMovementTest>();
-        player.talking = true;
-        StartCoroutine(Conversation());
-    }
-    */
-    
     private void Start() {
         StartCoroutine(Conversation());
     }
 
     IEnumerator Conversation() {
         BackgroundManager.UpdateBackground(outside2);
+        BackgroundManager.FadeIn();
+        yield return new WaitUntil(() => !BackgroundManager.isFading());
         intro.StartText();
         yield return new WaitUntil(() => intro.finished);
         question.StartQuestion();
@@ -34,7 +26,9 @@ public class MotorcycleNPC : MonoBehaviour {
         TextSpawner response = question.choice == 0 ? youExplode : youSurvive;
         response.StartText();
         yield return new WaitUntil(() => response.finished);
+        BackgroundManager.FadeOut();
+        yield return new WaitUntil(() => !BackgroundManager.isFading());
         BackgroundManager.UpdateBackground(outside);
-        // player.talking = false;
+        BackgroundManager.FadeIn();
     }
 }
