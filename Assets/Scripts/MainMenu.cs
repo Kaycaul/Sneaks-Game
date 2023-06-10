6907dee76f107c428d05436f7f85e1c59c5478a1
Kaycaul <84094849+Kaycaul@users.noreply.github.com>
2023-06-09 20:12:57 -0400

    MOVING BACKGROUND GRAHHH

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour {
 
-    [SerializeField] Sprite background;
     [SerializeField] AudioClip menuMusic;
+    [SerializeField] RawImage backgroundImage;
+    [SerializeField] Color fadeColor1, fadeColor2;
+    [SerializeField] Sprite guy;
 
     private void Start() {
+        StartCoroutine(BackgroundManager.FadeIn());
         AudioManager.PlayMusic(menuMusic);
-        StartCoroutine(BackgroundManager.FadeIn(background));
+    }
+
+    private void Update() {
+        // move the background
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // subtract this or it will go backward
+        backgroundImage.uvRect = new Rect(backgroundImage.uvRect.position - mousePos * Time.deltaTime * 0.1f, backgroundImage.uvRect.size);
+        // alternate between two colors
+        Color currentColor = Color.Lerp(fadeColor1, fadeColor2, Mathf.PingPong(Time.time / 3, 1));
+        backgroundImage.color = currentColor;
     }
 
     public void StartButtonPressed() {
@@ -18,6 +30,13 @@ public class MainMenu : MonoBehaviour {
     }
 
     public void QuitButtonPressed() {
+        StartCoroutine(Quit());
+    }
+
+    IEnumerator Quit() {
+        backgroundImage.gameObject.SetActive(false);
+        BackgroundManager.UpdateBackground(guy);
+        yield return BackgroundManager.FadeOut();
         Application.Quit();
     }
 
